8b3e502484fefa72e41c4d7d5f9a997b419c5c35
Uilan Souza <uilan.souza@valor.com.br>
2019-12-18 08:00:43 -0300

    adicionando a API de autores, não esquecer de apagar a pasta module e sqlite da api

--- reactive/curso-react-alura/src/App.js
+++ reactive/curso-react-alura/src/App.js
@@ -1,54 +1,52 @@
-import React, { Component, Fragment } from 'react';
-import 'materialize-css/dist/css/materialize.min.css'
-import Tabela from './Tabela';
-import Formulario from './Formulario';
-import Header from './Header'
-
-
+import React, { Component, Fragment } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import Tabela from "./Tabela";
+import Formulario from "./Formulario";
+import Header from "./Header";
 
 class App extends Component {
-
   state = {
     autores: [
       {
-        nome: 'Paulo',
-        livro: 'React',
-        preco: '1000'
+        nome: "Paulo",
+        livro: "React",
+        preco: "1000"
       },
       {
-        nome: 'Daniel',
-        livro: 'Java',
-        preco: '99'
+        nome: "Daniel",
+        livro: "Java",
+        preco: "99"
       },
       {
-        nome: 'Marcos',
-        livro: 'Design',
-        preco: '150'
+        nome: "Marcos",
+        livro: "Design",
+        preco: "150"
       },
       {
-        nome: 'Bruno',
-        livro: 'DevOps',
-        preco: '100'
+        nome: "Bruno",
+        livro: "DevOps",
+        preco: "100"
       }
-    ],
-  }
+    ]
+  };
 
   removeAutor = index => {
-
     const { autores } = this.state;
 
     this.setState({
       autores: autores.filter((autor, posAtual) => {
         return posAtual !== index;
-      }),
-    })
-
-  }
+      })
+    });
+  };
 
   escutadorDeSubmit = autor => {
     this.setState({ autores: [...this.state.autores, autor] });
-  }
+  };
   render() {
+    fetch("http://localhost:8000/api/autor")
+      .then(res => res.json())
+      .then(console.log);
     return (
       <Fragment>
         <Header />
