d96fdaf7a3a333c8120a16cdce8f991b747f5c93
Uilan Souza <uilan.souza@valor.com.br>
2020-01-07 08:46:48 -0300

    API Service ja implementado, dados estao sendo buscados pela API

--- reactive/curso-react-alura/src/ApiService.js
+++ reactive/curso-react-alura/src/ApiService.js
@@ -1,8 +1,31 @@
-ListaAutores: () => {
-    return fetch('http://localhost:8000/api/autor')
-    .then(res =>res.json())
-    }
-}
+const ApiService = {
+  ListaAutores: () => {
+    return fetch("http://localhost:8000/api/autor").then(res => res.json());
+  },
 
-export default ListaAutores;
+  CriaAutor: autor => {
+    return fetch("http://localhost:8000/api/autor", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: autor
+    });
+  },
 
+  ListaNomes: () => {
+    return fetch("http://localhost:8000/api/autor/nome").then(res =>
+      res.json()
+    );
+  },
+  ListaLivros: () => {
+    return fetch("http://localhost:8000/api/autor/livro").then(res =>
+      res.json()
+    );
+  },
+  RemoveAutor: id => {
+    return fetch(`http://localhost:8000/api/autor/${id}`, {
+      method: "DELETE",
+      Headers: { "content-type": "application/json" }
+    }).then(res => res.json());
+  }
+};
+export default ApiService;
--- reactive/curso-react-alura/src/App.js
+++ reactive/curso-react-alura/src/App.js
@@ -4,32 +4,36 @@ import Tabela from "./Tabela";
 import Formulario from "./Formulario";
 import Header from "./Header";
 import ApiService from "./ApiService";
+import PopUp from "./PopUp";
 
 class App extends Component {
-  state = {
-    autores: [
-      {
-        nome: "Paulo",
-        livro: "React",
-        preco: "1000"
-      },
-      {
-        nome: "Daniel",
-        livro: "Java",
-        preco: "99"
-      },
-      {
-        nome: "Marcos",
-        livro: "Design",
-        preco: "150"
-      },
-      {
-        nome: "Bruno",
-        livro: "DevOps",
-        preco: "100"
-      }
-    ]
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      autores: [
+        //   {
+        //     nome: "Paulo",
+        //     livro: "React",
+        //     preco: "1000"
+        //   },
+        //   {
+        //     nome: "Daniel",
+        //     livro: "Java",
+        //     preco: "99"
+        //   },
+        //   {
+        //     nome: "Marcos",
+        //     livro: "Design",
+        //     preco: "150"
+        //   },
+        //   {
+        //     nome: "Bruno",
+        //     livro: "DevOps",
+        //     preco: "100"
+        //   }
+      ]
+    };
+  }
 
   removeAutor = index => {
     const { autores } = this.state;
@@ -39,13 +43,24 @@ class App extends Component {
         return posAtual !== index;
       })
     });
+    PopUp.exibeMensagem("success", "Autor Removido com sucesso!");
   };
 
   escutadorDeSubmit = autor => {
     this.setState({ autores: [...this.state.autores, autor] });
+    PopUp.exibeMensagem("success", "Autor adicionado com sucesso!");
   };
+
+  componentDidMount() {
+    ApiService.ListaAutores().then(res => {
+      this.setState({ autores: [...this.state.autores, ...res.data] });
+    });
+  }
+
   render() {
-    const autores = ApiService.ListaAutores();
+    const autores = ApiService.ListaNomes();
+    console.log(autores);
+
     return (
       <Fragment>
         <Header />
